96eb6cc6b0fd73ba92a0d594d12582ab1a519593
akgupta5799 <21mcmc21@uohyd.ac.in>
2024-12-16 17:26:45 +0530

    Spring Security In-Memory Authentication

---
+++ Spring_Security_App/src/test/java/com/themathcode/SpringSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themathcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
